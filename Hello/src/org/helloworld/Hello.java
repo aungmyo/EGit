cdd305234ee4290c8b99c0737569be464323e52e
aungmyohtike <aungmyohtike@gmail.com>
2016-11-12 09:53:57 +0630

    3rd Commit

--- Hello/src/org/helloworld/Hello.java
+++ Hello/src/org/helloworld/Hello.java
@@ -3,7 +3,7 @@ package org.helloworld;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		System.out.println("https://github.com/aungmyo/first-repo.git");
 	}
 
 }
