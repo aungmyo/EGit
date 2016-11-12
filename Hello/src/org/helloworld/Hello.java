08cb6f2372893827554bb83807ad3a826bcd229d
aungmyohtike <aungmyohtike@gmail.com>
2016-11-12 10:08:04 +0630

    Create Set Up file

--- Hello/src/org/helloworld/Hello.java
+++ Hello/src/org/helloworld/Hello.java
@@ -3,7 +3,8 @@ package org.helloworld;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("https://github.com/aungmyo/first-repo.git");
+		System.out.println("HTTP: " + "https://github.com/aungmyo/Hello.git");
+		System.out.println("SSH: "+ "git@github.com:aungmyo/Hello.git");
 	}
 
 }
